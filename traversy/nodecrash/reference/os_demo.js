5ea80cf4f4043ba025e85aa5c7c8d31d6f5fedcd
chrisgiem <krzysztofgiemza1@gmail.com>
2019-08-11 13:58:07 +0200

    node crash 2

---
+++ traversy/nodecrash/reference/os_demo.js
@@ -0,0 +1,22 @@
+const os = require('os');
+
+// platform
+console.log(os.platform());
+
+// cpu arch
+console.log(os.arch());
+
+// CPU Core Info
+//console.log(os.cpus());
+
+// Free memory
+console.log(os.freemem());
+
+// Total memory
+console.log(os.totalmem());
+
+// home dir
+console.log(os.homedir());
+
+// uptime
+console.log(os.uptime());
