db3b1510a044670a4da8965d8666aa28a49f24d7
chrisgiem <krzysztofgiemza1@gmail.com>
2019-08-14 21:18:07 +0200

    node 3

--- traversy/nodecrash/index.js
+++ traversy/nodecrash/index.js
@@ -1,7 +1,32 @@
-const Person = require('./person');
-//import Person from './person'; nie
+//const Person = require('./person');
+////import Person from './person'; nie
+//
+////console.log(person.name);
+//const person1 = new Person('John Doe', 30);
+//
+//person1.greeting();
 
-//console.log(person.name);
-const person1 = new Person('John Doe', 30);
+///const Logger = require('./logger');
+///
+///const logger = new Logger();
+///
+///logger.on('message', data => console.log('Called Listener', data));
+///
+///logger.log('Hello World');
+///logger.log('Hi');
+///logger.log('Hello');
 
-person1.greeting();
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.end('<h1>Homepage</h1>');
+    }
+    //console.log(req.url);
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
