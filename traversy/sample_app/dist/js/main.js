c7e9a8ca76a1e376bd793051282b25afbae8cfc0
chrisgiem <krzysztofgiemza1@gmail.com>
2019-08-21 13:51:04 +0200

    2 code extesions

---
+++ traversy/sample_app/dist/js/main.js
@@ -0,0 +1,27 @@
+const name = 'Brad';
+console.log('logged output: name', name);
+
+// function name() {
+//   console.log(test);
+// }
+
+console.log('hello');
+console.log('error');
+
+const addValue = (num1, num2) => {
+  console.log(num1 + num2);
+  //   if() {
+  //       for() {
+  //           if() {
+
+  //           }
+  //       }
+  //   }
+};
+
+add(1, 2);
+
+const arr = [1, 2, 3, 4, 5];
+console.log('logged output: arr', arr);
+
+arr.forEach(num => console.log(num));
