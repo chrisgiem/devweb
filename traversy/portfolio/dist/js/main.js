dd25e953378f9d91f73f56505ea49a5faa7ade35
chrisgiem <krzysztofgiemza1@gmail.com>
2019-08-30 23:03:30 +0200

    portfolio 4

---
+++ traversy/portfolio/dist/js/main.js
@@ -0,0 +1,26 @@
+// select DOM items
+const menuBtn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// set initial state of menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+
+    // set menu state
+    showMenu = true;
+  } else {
+      
+  }
+}
