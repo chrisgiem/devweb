84ca48ed770cb15d3ec0b1922b620a425ab90316
chrisgiem <krzysztofgiemza1@gmail.com>
2019-08-31 21:59:13 +0200

    menu button, js & rotate

--- traversy/portfolio/dist/js/main.js
+++ traversy/portfolio/dist/js/main.js
@@ -1,5 +1,5 @@
 // select DOM items
-const menuBtn = document.querySelector('.btn-menu');
+const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
@@ -21,6 +21,13 @@ function toggleMenu() {
     // set menu state
     showMenu = true;
   } else {
-      
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    // set menu state
+    showMenu = false;
   }
 }
