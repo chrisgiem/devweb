4d9c313be9bb5d57d4b66a5124047da75a63be67
chrisgiem <krzysztofgiemza1@gmail.com>
2020-02-01 23:57:26 +0100

    sob ok chat

--- webdevsimplified/chat/server.js
+++ webdevsimplified/chat/server.js
@@ -5,7 +5,7 @@ const users = {};
 io.on('connection', socket => {
   socket.on('new-user', name => {
     users[socket.id] = name;
-    socket.broadcast.emit('użytkownik połączony', name);
+    socket.broadcast.emit('user-connected', name);
   });
   socket.on('send-chat-message', message => {
     socket.broadcast.emit('chat-message', {
