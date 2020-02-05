148453a66690b26beecb68d1c4581eb8d3eac807
chrisgiem <krzysztofgiemza1@gmail.com>
2020-02-05 17:14:26 +0100

    sroda 1

--- traversy/trchat/server.js
+++ traversy/trchat/server.js
@@ -8,4 +8,40 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db) {
   }
 
   console.log('MongoDB connected');
+
+  //connect do socket.io
+  client.on('connection', function() {
+    let chat = db.collection('chats');
+
+    //create function to send status
+    sendStatus = function(s) {
+      socket.emit('status', s);
+    };
+
+    //get chats from mongo collection
+    chat
+      .find()
+      .limit(100)
+      .sort({ _id: 1 })
+      .toArray(function(err, res) {
+        if (err) {
+          throw err;
+        }
+
+        //emit the messages
+        socket.emit('output', res);
+      });
+
+    //handle input events
+    socket.on('input', function(data) {
+      let name = data.name;
+      let message = data.message;
+
+      //check for name and message
+      if (name == '' || message == '') {
+        //send error status
+        sendStatus('podaj nick i wiadomość');
+      }
+    });
+  });
 });
