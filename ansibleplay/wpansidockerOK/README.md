ad4510cf6f2a18cb97053fdcfcaf3085b4d35c9e
chrisgiem <krzysztofgiemza1@gmail.com>
2020-03-11 18:52:33 +0100

    wp dockerized ok

--- ansibleplay/wpansidockerOK/README.md
+++ ansibleplay/wpansidockerOK/README.md
@@ -1,4 +1,5 @@
 https://www.youtube.com/watch?v=vFPOv3zn7eY&t=478s
+kompatybilne z apt based Debian i Ubuntu
 
 # Wordpress on Docker the correct way
 This project is designed to create a series of Docker containers running Wordpress. The modifier "correct way" is used because this process uses multiple containers instead of stuffing everything into one container. This makes the entire process more robust and extensible.
---
+++ overment/pieski/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+module.exports = app;
